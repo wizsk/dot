5c96b8a419df1eea4a512f220058c3a4158e853f
sakib <sakibul706@gmail.com>
2023-03-16 21:03:02 +0600

    idk

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,8 @@ vim.g.maplocalleader = ' '
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- cursor
+vim.opt.guicursor = ""
 -- cp pasete
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
