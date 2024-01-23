ce1bf688aa4d51b4e92ae4f8b1fabb0d00c495eb
Sakib <sakibul706@gmail.com>
2024-01-23 21:34:01 +0600

    hypr babyeee

--- .config/hypr/scripts/suspend.sh
+++ .config/hypr/scripts/suspend.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 swayidle -w \
-timeout 180 ' swaylock ' \
-timeout 180 ' hyprctl dispatch dpms off' \
+timeout 180 ' swaylock -f -c 000000 ' \
+timeout 210 ' hyprctl dispatch dpms off' \
 timeout 600 'systemctl suspend' \
 resume ' hyprctl dispatch dpms on' \
 before-sleep 'swaylock -f -c 000000'
