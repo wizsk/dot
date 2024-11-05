1c1d3b5d3f5aab54ee7699b790d9a4a08467f29b
Sakib <sakibul706@gmail.com>
2024-11-05 16:29:47 +0600

    fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,20 @@
-# bin
-export GOPATH="$HOME/.local/go"
-export GOPATH="$HOME/.local/bin"
-
 set fish_greeting
 
 if status is-interactive
     set -g fish_key_bindings fish_vi_key_bindings
     function fish_mode_prompt; end
 end
+
+if test -e "$HOME/.config/fish/local.fish"
+    source "$HOME/.config/fish/local.fish"
+end
+
+# PATH
+export GOPATH="$HOME/.local/go"
+export GOPATH="$HOME/.local/bin"
+
+# exports
+export FZF_DEFAULT_OPTS="--layout=reverse --height=30%"
 
 alias gd='pwd | wl-copy'
 alias g="git"
