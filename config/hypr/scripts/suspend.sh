dc61b56469831818087e5d3282aed78920c6e38b
Sakib <sakibul706@gmail.com>
2024-12-29 08:21:22 +0600

    hypr again!?

---
+++ config/hypr/scripts/suspend.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+swayidle -w timeout 180 ' swaylock -f -c 000000 ' timeout 210 ' hyprctl dispatch dpms off' timeout 600 'systemctl suspend' resume ' hyprctl dispatch dpms on' before-sleep 'swaylock -f -c 000000'
