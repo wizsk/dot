2501b3a6920793b790ee92ae0a4725e47b0610a5
Sakib <sakibul706@gmail.com>
2025-08-10 08:03:33 +0600

    alacritty theme changer

---
+++ config/alacritty/alacritty-theme.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+li="$HOME/.cache/alacritty-theme.toml"
+d="$HOME/.config/alacritty"
+td="$d/themes"
+
+# change it to default
+if [ "$1" = "d" ]; then
+	ln -fs "$td/rose_pine.toml" "$li"
+	exit
+fi
+
+t=$(ls "$td" | fzf)
+
+echo "$li -> $td/$t"
+
+ln -fs "$td/$t" "$li"
